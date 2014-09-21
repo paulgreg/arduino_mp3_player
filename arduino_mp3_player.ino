21427f1ce5678203e79ee0377bcfa41ee5f52c1d
Grégory Paul <paulgreg@gmail.com>
2014-09-21 19:21:18 +0200

    Some renaming...

--- arduino_mp3_player.ino
+++ arduino_mp3_player.ino
@@ -3,25 +3,26 @@
 #include <Adafruit_VS1053.h>
 #include <LiquidCrystal.h>
 
-#define BREAKOUT_RESET 13 // VS1053 reset pin (output)
-#define BREAKOUT_CS 10 // VS1053 chip select pin (output)
-#define BREAKOUT_DCS 8 // VS1053 Data/command select pin (output)
-#define DREQ 3 // VS1053 Data request pin (into Arduino)
-#define CARDCS 6 // Card chip select pin
+#define VS1053_RESET 13 // VS1053 reset pin (output)
+#define VS1053_CS 10 // VS1053 chip select pin (output)
+#define VS1053_DCS 8 // VS1053 Data/command select pin (output)
+#define VS1053_DREQ 3 // VS1053 Data request pin (into Arduino)
+#define VS1053_CARDCS 6 // Card chip select pin
 
-Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, VS1053_CARDCS);
 LiquidCrystal lcd(A0, A1, A5, A4, A3, A2);
 
-#define UP 0
-#define LEFT 1
-#define DOWN 2
-#define RIGHT 3
-#define PRESS 4
-#define NO_ACTION 5
+#define NO_ACTION 0
+#define UP 1
+#define LEFT 2
+#define DOWN 3
+#define RIGHT 4
+#define PRESS 5
 
 #define PAUSE_AFTER_ACTION 250
 
 #define LCD true
+#define SERIAL true
 
 void setup() {
   pinMode(A0, OUTPUT);
@@ -41,7 +42,7 @@ void setup() {
     while (1); // don't do anything more
   }
 
-  if (!SD.begin(CARDCS)) {
+  if (!SD.begin(VS1053_CARDCS)) {
     show("SD card not found");
     while (1); // don't do anything more
   }
@@ -56,11 +57,11 @@ void setup() {
     show("DREQ pin not irq");
   }
 
-  musicPlayer.GPIO_pinMode(0, INPUT);
-  musicPlayer.GPIO_pinMode(1, INPUT);
-  musicPlayer.GPIO_pinMode(2, INPUT);
-  musicPlayer.GPIO_pinMode(3, INPUT);
-  musicPlayer.GPIO_pinMode(4, INPUT);
+  musicPlayer.GPIO_pinMode(UP, INPUT);
+  musicPlayer.GPIO_pinMode(DOWN, INPUT);
+  musicPlayer.GPIO_pinMode(LEFT, INPUT);
+  musicPlayer.GPIO_pinMode(RIGHT, INPUT);
+  musicPlayer.GPIO_pinMode(PRESS, INPUT);
 
   // Launch music at first
   musicPlayer.startPlayingFile("track001.mp3");
@@ -73,14 +74,15 @@ boolean last_action;
 void loop() {
 
   action = NO_ACTION;
+
   // Check inputs
-  for (action = 0; action < 5; action++) { 
-    delay(50); // Wait a little bit between digital pin read
-    if (musicPlayer.GPIO_digitalRead(action) == 1) {    
+  for (uint8_t i = 1; i < 6; i++) { 
+    if (musicPlayer.GPIO_digitalRead(i) == HIGH) {    
+      action = i;
       break;
     }
+    delay(50); // Wait a little bit between digital pin read
   }
-
 
 
   switch(action) {
@@ -109,13 +111,13 @@ void loop() {
     if (!musicPlayer.paused()) {
       musicPlayer.pausePlaying(true);
       displayPause();
-      } 
+    } 
     else {
       musicPlayer.pausePlaying(false);
       hidePause();
-      }
+    }
 
-      afterAction();
+    afterAction();
     break;
 
   case NO_ACTION:
@@ -133,6 +135,9 @@ void afterAction() {
   last_action = true;
   delay(PAUSE_AFTER_ACTION);
 }
+
+
+
 
 
 
