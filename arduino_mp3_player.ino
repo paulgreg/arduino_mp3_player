c0f4501c29d2268adf4826cddc6abacae5cbd67c
Grégory Paul <paulgreg@gmail.com>
2014-09-27 17:25:57 +0200

    Use arduino to read 5 way button via multiple resistors...
    See http://www.instructables.com/id/Accessing-5-buttons-through-1-Arduino-pin-Revisi/?ALLSTEPS

--- arduino_mp3_player.ino
+++ arduino_mp3_player.ino
@@ -14,40 +14,36 @@
 
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, VS1053_CARDCS);
 
-
+// Software SPI
 #define OLED_MOSI   9
 #define OLED_CLK   10
 #define OLED_DC    11
 #define OLED_CS    12
 #define OLED_RESET 13
+
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-
 /*
+// Hardware SPI
 #define OLED_DC     7
 #define OLED_CS     11
 #define OLED_RESET  13
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 */
 
-#define UP 1
-#define LEFT 2
-#define DOWN 4
-#define RIGHT 8
-#define PRESS 16
-
 #define PAUSE_AFTER_ACTION 250
 
-#define SERIAL true
 #define OLED true
 
 uint8_t volume = 20;
 
 void setup() {
 
-#ifdef SERIAL
   Serial.begin(9600);
-#endif
+
+  // 5 Way button
+  pinMode(A0, INPUT);
+
 
 #ifdef OLED
   display.begin(SSD1306_SWITCHCAPVCC);
@@ -56,114 +52,98 @@ void setup() {
   display.clearDisplay();
 #endif
 
-  if (!musicPlayer.begin()) {
-    show("VS1053 not found");
+  if (!musicPlayer.begin() || !SD.begin(VS1053_CARDCS)) {
+    show("VS1053 or SDCARD not found");
     while (1); // don't do anything more
   }
-
-  if (!SD.begin(VS1053_CARDCS)) {
-    show("SD card not found");
-    while (1); // don't do anything more
-  }
-  show("SD card found");
-
-  // list files
-  printDirectory(SD.open("/"), 0);
-
-  musicPlayer.setVolume(volume, volume); // Set volume for left, right channels. lower numbers == louder volume!
-
-  if (! musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT)) {
+  if (!musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT)) {
     show("DREQ pin not irq");
   }
 
-  musicPlayer.GPIO_pinMode(UP, INPUT);
-  musicPlayer.GPIO_pinMode(DOWN, INPUT);
-  musicPlayer.GPIO_pinMode(LEFT, INPUT);
-  musicPlayer.GPIO_pinMode(RIGHT, INPUT);
-  musicPlayer.GPIO_pinMode(PRESS, INPUT);
+  musicPlayer.setVolume(volume, volume); // Set volume for left, right channels. lower numbers == louder volume!
+
+  // list files
+  printDirectory(SD.open("/"), 0);
 
   // Launch music at first
   musicPlayer.startPlayingFile("track002.mp3");
   show("Playing track002");
 }
 
-boolean last_action;
+// Globals
 
 void loop() {
 
-    // Check inputs
-    uint16_t action = musicPlayer.GPIO_digitalRead();
+  // Check inputs
+  uint16_t action = analogRead(A0);
 
-    if (action & UP) {
-      showAtXY("UP   ", 10, 1);
+Serial.println(action);
 
-      if (volume > 0) {
-        volume--;
-      }
-      musicPlayer.setVolume(volume,volume);
-
-      afterAction();
-
+  if (action > 940 && action < 960) {
+    if (volume < 20) {
+      volume++;
     }
-    if (action & DOWN) {
-      showAtXY("DOWN ", 10, 1);
+    char buffer[3];
+    snprintf(buffer, 3, "%02i", volume);
+    show(buffer);
+    
+    musicPlayer.setVolume(volume, volume);
 
-      if (volume < 20) {
-        volume++;
-      }
-      musicPlayer.setVolume(volume,volume);
-
-      afterAction();
+    afterAction();
+  }
+  if (action > 750 && action < 780) {
+    if (volume > 0) {
+      volume--;
     }
-    if (action & LEFT) {
-      showAtXY("LEFT ", 10, 1);
+    char buffer[3];
+    snprintf(buffer, 3, "%02i", volume);
+    show(buffer);
 
-      musicPlayer.stopPlaying();
-      delay(50);
-      musicPlayer.startPlayingFile("track002.mp3");
-      show("Playing track002");
+    musicPlayer.setVolume(volume, volume);
+    afterAction();
+  }
+  if (action > 910 && action < 940) {
+    show("LEFT");
+
+    musicPlayer.stopPlaying();
+    delay(50);
+    musicPlayer.startPlayingFile("track002.mp3");
+    show("Playing track002");
+    hidePause();
+
+    afterAction();
+  }
+  if (action > 1000 && action < 1040) {
+    show("RIGHT");
+
+    musicPlayer.stopPlaying();
+    delay(50);
+    musicPlayer.startPlayingFile("track001.mp3");
+    show("Playing track001");
+    hidePause();
+
+    afterAction();
+  }
+  if (action > 820 && action < 860) {
+    show("PRESS");
+
+    if (!musicPlayer.paused()) {
+      musicPlayer.pausePlaying(true);
+      displayPause();
+    }
+    else {
+      musicPlayer.pausePlaying(false);
       hidePause();
-
-      afterAction();
     }
-    if (action & RIGHT) {
-      showAtXY("RIGHT", 10, 1);
 
-      musicPlayer.stopPlaying();
-      delay(50);
-      musicPlayer.startPlayingFile("track001.mp3");
-      show("Playing track001");
-      hidePause();
-
-      afterAction();
-    }
-    if (action & PRESS) {
-      showAtXY("PRESS", 10, 1);
-
-      if (!musicPlayer.paused()) {
-        musicPlayer.pausePlaying(true);
-        displayPause();
-      }
-      else {
-        musicPlayer.pausePlaying(false);
-        hidePause();
-      }
-
-      afterAction();
-    }
-    if  (action == 0) {
-      if (last_action == true) {
-        showAtXY("     ", 10, 1);
-        last_action = false;
-      }
-    }
+    afterAction();
+  }
   
-  displayElapsedTime();
-  delay(100);
+  updateDisplay();
+  delay(50);
 }
 
 void afterAction() {
-  last_action = true;
   delay(PAUSE_AFTER_ACTION);
 }
 
--- display.ino
+++ display.ino
@@ -1,23 +1,12 @@
 
 void show(char * m) {
-  showAtXY(m, 0, 0);
-}
-
-void showatY(char * m, uint8_t y) {
-  showAtXY(m, 0, y);
-}
-
-void showAtXY(char * m, uint8_t x, uint8_t y) {
 #ifdef OLED
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(x, y);
-  display.print(m);
+  display.setCursor(0, 0);
+  display.println(m);
   display.display();
-#endif
-#ifdef SERIAL
-  Serial.println(m);
 #endif
 }
 
@@ -26,22 +15,22 @@ char* formatTime(char* buffer, uint16_t time) {
   uint8_t minutes = ((time % 3600) / 60);
   uint8_t seconds = ((time % 3600) % 60);
 
-  snprintf(buffer, 10, "%02i:%02i:%02i", hours, minutes, seconds);
+  snprintf(buffer, 9, "%02i:%02i:%02i", hours, minutes, seconds);
 }
 
-void displayElapsedTime() {
-  char timebuffer[10];
+void updateDisplay() {
+  char timebuffer[9];
   formatTime(timebuffer, musicPlayer.decodeTime());
-  showAtXY(timebuffer, 1, 1);
+  show(timebuffer);
 }
 
 
 void displayPause() {
-  showAtXY("\"", 0, 1);
+  show("\"");
 }
 
 void hidePause() {
-  showAtXY(" ", 0, 1);
+  show(" ");
 }
 
 
